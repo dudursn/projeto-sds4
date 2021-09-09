a88a2c666e80cd382b3d7611548a0269a818b4a3
Eduardo Nascimento <roger.rsn@outlook.com>
2021-09-09 11:50:29 -0300

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/configs/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsvendas/configs/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsvendas.config;
+package com.devsuperior.dsvendas.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dsvendas.services;
+
+import com.devsuperior.dsvendas.dtos.SellerDTO;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SellerService {
+
+    @Autowired
+    private SellerRepository repository;
+
+    public List<SellerDTO> findAll() {
+
+        List<Seller> result = repository.findAll();
+
+        //Cada elemento da lista original é convertida para a classe alvo DTO e depois a stream é convertida para lista
+        return result.stream().map(data -> new SellerDTO(data)).collect(Collectors.toList());
+    }
+}
