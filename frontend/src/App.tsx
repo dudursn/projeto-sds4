b9831442a40d4f9515fdaf3c846a70e4770f397f
Eduardo Nascimento <roger.rsn@outlook.com>
2021-09-11 11:25:50 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,9 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
-import Navbar from "components/Navbar";
+import Routes from "Routes";
+
 
 function App() {
     return (
-        <>
-            <Navbar />
-            <div className="container">
-
-                <h1 className="text-primary py-3">Dashboard de Vendas!</h1>
-
-                <div className="row px-3">
-                    <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Porcentagem de Sucesso nas Vendas</h5>
-                        <BarChart />
-                    </div>
-                    <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Porcentagem de Vendas</h5>
-                        <DonutChart />
-                    </div>
-                </div>
-
-                <div className="py-3">
-                    <h5 className="text-secondary">Todas as Vendas</h5>
-                    <DataTable />
-                </div>
-                
-            </div>
-            <Footer />
-        </>
+        <Routes />
     );
 }
 
